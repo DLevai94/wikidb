f66a28f1574340f5248f1f77a97cfa48181d26f3
David Levai <david@davidlevai.com>
2020-09-01 12:48:13 +0200

    some small fixes

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
 import AppBar from './Appbar';
+import useStyles from '../hooks/useStyles';
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
-    <>
+    <div className={classes.root}>
       <AppBar />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
--- hooks/useMovies.js
+++ hooks/useMovies.js
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 
 function useMovies(title) {
-  const { data, error } = useSWR(`/api/movies?title=${title}`);
+  const { data, error } = useSWR(title ? `/api/movies?title=${title}` : null);
   return {
     movies: data?.Search,
     isLoading: !error && !data,
